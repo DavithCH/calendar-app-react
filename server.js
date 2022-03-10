b86fc906a8b318d19eb2676bf600c539a3ea33fc
Davith Chhung <chhungdavith@gmail.com>
2022-03-10 12:11:31 +0100

    fixed

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router(__dirname + "/build/db/db.json");
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router(__dirname + "/build/db/db.json");
 
-const middleware = jsonServer.defaults;
-server.use(middleware);
-server.use(router);
+// const middleware = jsonServer.defaults;
+// server.use(middleware);
+// server.use(router);
 
-const serverPort = process.env.PORT || 3333;
-server.listen(serverPort, () =>
-  console.log("json server is running on port" + serverPort)
-);
+// const serverPort = process.env.PORT || 3333;
+// server.listen(serverPort, () =>
+//   console.log("json server is running on port" + serverPort)
+// );
 app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
